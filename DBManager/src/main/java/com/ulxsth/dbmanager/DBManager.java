dfb99fcc0face1e8115d400ecd9724908bb21181
ulxsth <ulxsth@gmail.com>
2023-05-21 16:17:17 +0900

    update: DB管理用のプラグインを作成

---
+++ DBManager/src/main/java/com/ulxsth/dbmanager/DBManager.java
@@ -0,0 +1,17 @@
+package com.ulxsth.dbmanager;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class DBManager extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
