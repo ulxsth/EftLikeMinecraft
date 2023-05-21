9f3ad47b1da528b130dcb4c3f887f03841318b49
ulxsth <ulxsth@gmail.com>
2023-05-21 20:18:55 +0900

    update: DBManagerの名前がクラス名と干渉しそうだったので変更

--- DatabaseAPI/src/main/java/com/ulxsth/databaseapi/DatabaseAPI.java
+++ DatabaseAPI/src/main/java/com/ulxsth/databaseapi/DatabaseAPI.java
@@ -1,8 +1,8 @@
-package com.ulxsth.dbmanager;
+package com.ulxsth.databaseapi;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class DBManager extends JavaPlugin {
+public final class DatabaseAPI extends JavaPlugin {
 
     @Override
     public void onEnable() {
