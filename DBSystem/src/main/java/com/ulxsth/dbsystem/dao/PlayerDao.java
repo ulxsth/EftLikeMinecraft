7ce0940d9e81d606da81ce595caf20f58beb91ca
ulxsth <ulxsth@gmail.com>
2023-05-23 10:53:00 +0900

    add: PlayerDaoの追加

--- DBSystem/src/main/java/com/ulxsth/dbsystem/dao/PlayerDao.java
+++ DBSystem/src/main/java/com/ulxsth/dbsystem/dao/PlayerDao.java
@@ -1,2 +1,43 @@
-package com.ulxsth.dbsystem.dao;public class PlayerDao {
+package com.ulxsth.dbsystem.dao;
+
+import com.ulxsth.dbsystem.DBSystem;
+import com.ulxsth.dbsystem.dto.PlayerDto;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.UUID;
+
+public class PlayerDao {
+    private static final DBSystem plugin = DBSystem.getInstance();
+
+    public static final String PATH = "jdbc:sqlite:" + plugin.getDataFolder() + "\\data.db";
+    public static final String TABLE_NAME = "players";
+    private Connection connection;
+
+    PlayerDao() {
+        try {
+            this.connection = DriverManager.getConnection(PATH);
+            Statement statement = connection.createStatement();
+
+            // dbの作成
+            String sql = "CREATE TABLE IF NOT EXISTS " +
+                    TABLE_NAME +
+                    "(unique_id CHAR(36) PRIMARY KEY," +
+                    "name VARCHAR(16)," +
+                    "first_logged_in DATE," +
+                    "recently_logged_in DATE);";
+            statement.executeUpdate(sql);
+            connection.close();
+        } catch(SQLException err) {
+            err.printStackTrace();
+        }
+    }
+
+    public void insert(PlayerDto playerDto) throws SQLException {}
+    public PlayerDto selectByUniqueId(UUID uniqueId) throws SQLException {}
+    public void update(PlayerDto playerDto) throws SQLException {}
+    public void upsert(PlayerDto playerDto) throws SQLException {}
+    public void delete(UUID uniqueId) throws SQLException {}
 }
