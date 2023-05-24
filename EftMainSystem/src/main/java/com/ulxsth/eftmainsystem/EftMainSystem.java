2f48a6ee149fd7cd734fa7fd2e966b99baebece7
ulxsth <ulxsth@gmail.com>
2023-05-25 04:32:38 +0900

    update: メインクラスをシングルトン化

--- EftMainSystem/src/main/java/com/ulxsth/eftmainsystem/EftMainSystem.java
+++ EftMainSystem/src/main/java/com/ulxsth/eftmainsystem/EftMainSystem.java
@@ -3,9 +3,17 @@ package com.ulxsth.eftmainsystem;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class EftMainSystem extends JavaPlugin {
+    private static EftMainSystem instance = null;
+
+    public static EftMainSystem getInstance() {
+        if(instance == null) throw new IllegalStateException("メインクラスが初期化されていません");
+        return instance;
+    }
 
     @Override
     public void onEnable() {
-        this.getServer().getPluginManager().registerEvents(new EventListener(), this);
+        instance = this;
+
+        this.getServer().getPluginManager().registerEvents(new EventListener(), instance);
     }
 }
