2b15187db97c0db10d17522543c715e2ee8e20dc
ulxsth <ulxsth@gmail.com>
2023-05-25 04:42:12 +0900

    update: 実装

---
+++ EftMainSystem/src/main/java/com/ulxsth/eftmainsystem/db/PlayerDto.java
@@ -0,0 +1,21 @@
+package com.ulxsth.eftmainsystem.db;
+
+import java.util.Date;
+import java.util.UUID;
+
+public record PlayerDto(UUID uniqueId, String name, Date firstLoggedIn, Date recentlyLoggedIn) {
+    public PlayerDto {
+        if(name.length() == 0 || name.length() > 16) {
+            throw new IllegalArgumentException("nameの長さは1文字以上16文字以下である必要があります");
+        }
+    }
+
+    public PlayerDto setName(String name) {
+        return new PlayerDto(
+                uniqueId(),
+                name,
+                firstLoggedIn(),
+                recentlyLoggedIn()
+        );
+    }
+}
