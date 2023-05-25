4942ccf39a6e1d8b7b6f6690a15b1476391442f3
ulxsth <ulxsth@gmail.com>
2023-05-25 11:59:46 +0900

    update: メインクラスをシングルトン化

--- EftEconomySystem/src/main/java/com/ulxsth/efteconomysystem/EftEconomySystem.java
+++ EftEconomySystem/src/main/java/com/ulxsth/efteconomysystem/EftEconomySystem.java
@@ -3,14 +3,16 @@ package com.ulxsth.efteconomysystem;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class EftEconomySystem extends JavaPlugin {
+    private static EftEconomySystem plugin = null;
 
-    @Override
-    public void onEnable() {
-        this.getServer().getPluginManager().registerEvents(new EventListener(), this);
+    public static EftEconomySystem getInstance() {
+        if(plugin == null) throw new IllegalStateException("メインクラスが初期化されていません");
+        return plugin;
     }
 
     @Override
-    public void onDisable() {
-        // Plugin shutdown logic
+    public void onEnable() {
+        plugin = this;
+        this.getServer().getPluginManager().registerEvents(new EventListener(), this);
     }
 }
