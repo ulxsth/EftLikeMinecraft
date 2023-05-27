c75471eba355d1269ead8d7b7777868dcfeace3c
ulxsth <ulxsth@gmail.com>
2023-05-27 21:32:40 +0900

    update: 初ログイン時のDB初期化処理を実装

--- EftEconomySystem/src/main/java/com/ulxsth/efteconomysystem/EventListener.java
+++ EftEconomySystem/src/main/java/com/ulxsth/efteconomysystem/EventListener.java
@@ -1,7 +1,47 @@
 package com.ulxsth.efteconomysystem;
 
+import com.ulxsth.efteconomysystem.db.MoneyDao;
+import com.ulxsth.efteconomysystem.db.MoneyDto;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+
+import java.sql.SQLException;
+import java.util.UUID;
 
 public class EventListener implements Listener {
+    private static final EftEconomySystem plugin = EftEconomySystem.getInstance();
 
+    @EventHandler
+    public void onJoin(PlayerJoinEvent event) {
+        MoneyDao dao = null;
+        try {
+            dao = new MoneyDao();
+        } catch(SQLException err) {
+            err.printStackTrace();
+        }
+        if(dao == null) {
+            plugin.getLogger().info("データベースのアクセスに失敗しました。");
+            return;
+        }
+
+        Player player = event.getPlayer();
+        UUID uniqueId = player.getUniqueId();
+
+        if(dao.isExist(uniqueId)) {
+            return;
+        }
+
+        MoneyDto dto = new MoneyDto(
+                uniqueId,
+                0
+        );
+
+        try {
+            dao.insert(dto);
+        } catch (SQLException err) {
+            err.printStackTrace();
+        }
+    }
 }
