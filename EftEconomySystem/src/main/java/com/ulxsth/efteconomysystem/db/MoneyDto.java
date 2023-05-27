ebbdcb28ab96ab5c600b206d68737555e70c3152
ulxsth <ulxsth@gmail.com>
2023-05-27 20:57:24 +0900

    rename

--- EftEconomySystem/src/main/java/com/ulxsth/efteconomysystem/db/MoneyDto.java
+++ EftEconomySystem/src/main/java/com/ulxsth/efteconomysystem/db/MoneyDto.java
@@ -2,29 +2,29 @@ package com.ulxsth.efteconomysystem.db;
 
 import java.util.UUID;
 
-public record MoneysDto(UUID uniqueId, int amount) {
-    public MoneysDto {
+public record MoneyDto(UUID uniqueId, int amount) {
+    public MoneyDto {
         if(amount < 0) {
             throw new IllegalArgumentException("amountは整数値である必要があります");
         }
     }
 
-    public MoneysDto setAmount(int newAmount) {
-        return new MoneysDto(
+    public MoneyDto setAmount(int newAmount) {
+        return new MoneyDto(
                 this.uniqueId,
                 newAmount
         );
     }
 
-    public MoneysDto addAmount(int addAmount) {
-        return new MoneysDto(
+    public MoneyDto addAmount(int addAmount) {
+        return new MoneyDto(
                 this.uniqueId,
                 this.amount + addAmount
         );
     }
 
-    public MoneysDto decAmount(int decAmount) {
-        return new MoneysDto(
+    public MoneyDto decAmount(int decAmount) {
+        return new MoneyDto(
                 this.uniqueId,
                 this.amount - decAmount
         );
