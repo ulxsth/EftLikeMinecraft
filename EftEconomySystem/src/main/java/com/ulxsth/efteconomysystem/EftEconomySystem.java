73c11d352614c0dfc76188e871db0730b073f2ec
ulxsth <ulxsth@gmail.com>
2023-05-28 15:25:52 +0900

    fix: コンストラクタを秘匿化

--- EftEconomySystem/src/main/java/com/ulxsth/efteconomysystem/EftEconomySystem.java
+++ EftEconomySystem/src/main/java/com/ulxsth/efteconomysystem/EftEconomySystem.java
@@ -5,6 +5,8 @@ import org.bukkit.plugin.java.JavaPlugin;
 public final class EftEconomySystem extends JavaPlugin {
     private static EftEconomySystem plugin = null;
 
+    private EftEconomySystem() {}
+
     public static EftEconomySystem getInstance() {
         if(plugin == null) throw new IllegalStateException("メインクラスが初期化されていません");
         return plugin;
