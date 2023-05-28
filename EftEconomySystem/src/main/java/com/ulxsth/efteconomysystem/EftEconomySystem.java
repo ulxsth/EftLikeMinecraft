9e803223e3c03c44573b8843fe5c9c92538ddf7e
ulxsth <ulxsth@gmail.com>
2023-05-28 16:13:31 +0900

    update: /stats を登録

--- EftEconomySystem/src/main/java/com/ulxsth/efteconomysystem/EftEconomySystem.java
+++ EftEconomySystem/src/main/java/com/ulxsth/efteconomysystem/EftEconomySystem.java
@@ -1,11 +1,10 @@
 package com.ulxsth.efteconomysystem;
 
+import com.ulxsth.efteconomysystem.commands.StatsCommandExecutor;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class EftEconomySystem extends JavaPlugin {
     private static EftEconomySystem plugin = null;
-
-    private EftEconomySystem() {}
 
     public static EftEconomySystem getInstance() {
         if(plugin == null) throw new IllegalStateException("メインクラスが初期化されていません");
@@ -16,5 +15,6 @@ public final class EftEconomySystem extends JavaPlugin {
     public void onEnable() {
         plugin = this;
         this.getServer().getPluginManager().registerEvents(new EventListener(), this);
+        getCommand("stats").setExecutor(new StatsCommandExecutor());
     }
 }
